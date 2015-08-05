af35865d57e259384541bdab196fc20c643efc85
vnatorf <vnatorf@gmail.com>
2015-08-05 00:18:42 -0300

    especificacaco

---
+++ especificacao/estetica-rua-praia/src/main/java/br/com/erp/EsteticaRuaPraiaApplication.java
@@ -0,0 +1,12 @@
+package br.com.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticaRuaPraiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsteticaRuaPraiaApplication.class, args);
+    }
+}
